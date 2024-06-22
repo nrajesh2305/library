d317c422a3b1551ce193d8e845e9bdcdbf669ada
nrajesh2305 <rajeshnithin2305@csu.fullerton.edu>
2024-06-22 14:42:26 -0700

    Finished adding an add book button.

---
+++ script.js
@@ -0,0 +1,15 @@
+const myLibrary = new Array();
+
+function Book(title, author, num_pages, is_read)
+{
+    // the constructor.
+    this.title = title;
+    this.author = author;
+    this.num_pages = num_pages;
+    this.is_read = is_read;
+}
+
+function addBookToLibrary()
+{
+    // on the press of the button on the screen, add the book to the library.
+}
