dad50484563aac44121f852ea95dfb0e37b9b2c3
nrajesh2305 <rajeshnithin2305@csu.fullerton.edu>
2024-06-25 11:11:24 -0700

    Finished adding a dom feature for the shelf to pop up, but there are bugs here. Just a checkpoint.

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const myLibrary = new Array();
 const add_book_button = document.querySelector("button.add-book");
+const book_form = document.querySelector("#book-form");
 const form = document.querySelector("form.add-book-form");
 const is_read_button = document.querySelector("#is_read");
 is_read_button.style.backgroundColor = "red";
 const submit_button = document.querySelector("#submit");
 let title = document.querySelector("#title");
+let author = document.querySelector("#author");
+let numPages = document.querySelector("#numPages");
+const shelf_area = document.querySelector(".shelf-area");
 
 function Book(title, author, num_pages, is_read)
 {
@@ -33,10 +37,29 @@ is_read_button.addEventListener("click", () =>
     }
 });
 
+submit_button.addEventListener("submit", function(event)
+{
+    if(title.textContent != null && author.textContent != null && numPages.textContent != null)
+    {
+        event.preventDefault();
+        this.reset();  
+    }
+});
+
 function addBookToLibrary()
 {
     // on the press of the button on the screen, add the book to the library.
     let book = this.title.value;
     myLibrary.push(book);
     return myLibrary;
+}
+
+function addShelf()
+{
+    // Add a shelf on top of the other shelves if the amount of books in the current shelf
+    // has 8 books, and if so, the 9th book they are trying to add with now be put into the top shelf
+    // above it.
+    new_div = document.createElement("div");
+    shelf_area.appendChild(new_div);
+    return shelf_area;
 }
